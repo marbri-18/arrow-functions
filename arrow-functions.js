96ae62db66605dbbe8602ddd29f9313465e1054e
Martin Bridgman <martin.bridgman@gmail.com>
2022-01-09 14:58:16 +0000

    Arrow function with parameters

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,8 +4,19 @@
  */
 
 // Vanilla JavaScript Function
-
+   /*  function addTwoNumbers (a, b){
+        //code block
+        return  a + b;
+    }
+    sum = addTwoNumbers(6, 5);
+    console.log(sum); */
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    //code block
+    return  a + b;
+}
+sum = addTwoNumbers(9, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
