4f162d74fbb2b1f7c9dfb30ff828a1940dacd518
Martin Bridgman <martin.bridgman@gmail.com>
2022-01-25 22:36:43 +0000

    Complete lecture

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,22 +4,37 @@
  */
 
 // Vanilla JavaScript Function
-   /*  function addTwoNumbers (a, b){
+    function addTwoNumbers (a, b){
         //code block
         return  a + b;
     }
     sum = addTwoNumbers(6, 5);
-    console.log(sum); */
+    console.log(sum);
 // Arrow Function With Parameters
-const addTwoNumbers = (a, b) => {
+const addTwoNumbers1 = (a, b) => {
     //code block
     return  a + b;
 }
-sum = addTwoNumbers(9, 5);
-console.log(sum);
+sum1 = addTwoNumbers1(9, 5);
+console.log(sum1);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(4, 9);
+console.log(sum2);
+
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello World!");
+
+const sayHello = () => console.log("Hello again World!");
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multi line string!
+    </p>`
+)
+console.log(returnMultipleLines());
